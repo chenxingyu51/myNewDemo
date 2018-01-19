a078031c3bed9f65a9d4cfdd2900ea4f4cf56815
chenxingyu <chenxingyu_51@163.com>
2018-01-19 11:06:32 +0800

    第一次提交model

---
+++ bezier/model.h
@@ -0,0 +1,13 @@
+//
+//  model.h
+//  bezier
+//
+//  Created by MAC on 2025/1/19.
+//  Copyright © 2018年 MAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface model : NSObject
+
+@end
---
+++ bezier/model.m
@@ -0,0 +1,13 @@
+//
+//  model.m
+//  bezier
+//
+//  Created by MAC on 2025/1/19.
+//  Copyright © 2018年 MAC. All rights reserved.
+//
+
+#import "model.h"
+
+@implementation model
+
+@end
